9a8c1f77e503a3e2e5f53eba3c37c173660c713d
Kunal <sharmakunal19806@gmail.com>
2026-08-27 02:20:24 +0530

    some_changes

--- frontend/src/features/auth/services/auth.api.js
+++ frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import { applyMiddleware } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:3000/api";
+const API_BASE_URL = "/api";
 
 // Create axios instance
 const api = axios.create({
